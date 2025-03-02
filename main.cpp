caed87b3146b0a201e43c88900f88663bed64462
IsaiahBideshi <isaiahbideshi@gmail.com>
2025-03-02 17:42:01 -0400

    Did Alot: - Added MAX_ASTEROIDS variable, allocates that amount of space asteroids
    - Added a godMode for test, when true, player does not lose lives
    
    - At 60 seconds elapsed, only small asteroids spawn, at 30 seconds elapsed only small and medium spawn and before 30 seconds no small spawn.
    
    - removed "spawnrate" attribute from asteroid class and created a variable in main, asteroidSpawnRate.
    
    - The heart pngs got deleted somehow, I redid them, they look a bit different.
    
    - Weird error with bullet 7 x coord gets changed, fix on line 307.
    
    - Changed points awarded based on size of asteroid:
      Small - 50 pts
      Medium - 30 pts
      Large - 10 pts.
    
    - Added timer at top of screen: overallTimer, timerText.
    
    TODO in this branch:
    - Add health feature, when shooting a large asteroid, dont kill instantly but remove some health and slow down.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
 //  Ammo system shoot and reload
-/*  Game over screen not working:
- *      - Freezes and stops responding after some time
- *      - AI says to not use a while loop for game over screen and use an if statement instead
-*/
 
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
@@ -36,7 +32,7 @@ public:
 
 class asteroid {
 public:
-    int size = rand() % 3, health = size + 1, spawnRate = 1000;
+    int size = rand() % 3, health = size + 1;
     float x = -100, y = -100;
 
     sf::Sprite asteroidSprite;
@@ -103,10 +99,13 @@ public:
 };
 
 int main(){
+    int bulletCount = 0, asteroidIndex = 0, asteroidSpeed = 3, MAX_ASTEROIDS = 500, asteroidSpawnRate = 1000;
+    bool isGameOver = true, godMode = true;
+
     sf::RenderWindow window(sf::VideoMode(480 , 800), "Space Invaders");
     player player1;
-    sf::Clock cooldown, asteroidSpawn, speedTimer, scoreTimer;
-    asteroid asteroids[100];
+    sf::Clock cooldown, asteroidSpawn, speedTimer, overallTimer;
+    asteroid asteroids[MAX_ASTEROIDS];
 
     sf::Texture fullHeart, halfHeart, emptyHeart;
     fullHeart.loadFromFile("full-heart1.png");
@@ -122,10 +121,14 @@ int main(){
 //  Text:
     sf::Font font;
     font.loadFromFile("arial.ttf");
-    sf::Text scoreText("",font), titleText("Space Invaders", font), startText("Start", font);
+    sf::Text scoreText("",font), titleText("Space Invaders", font), startText("Start", font), timerText("", font);
     scoreText.setCharacterSize(30);
     scoreText.setOutlineColor(sf::Color::Black);
     scoreText.setOutlineThickness(2);
+
+    timerText.setCharacterSize(30);
+    timerText.setOutlineColor(sf::Color::Black);
+    timerText.setOutlineThickness(2);
 
 
     titleText.setStyle(sf::Text::Bold);
@@ -141,11 +144,9 @@ int main(){
     blackBackground.setFillColor(sf::Color::Black);
     sf::RectangleShape restartButton(sf::Vector2f(100, 30));
 
-    int bulletCount = 0, asteroidIndex = 0, asteroidSpeed = 3;
-    bool isGameOver = true;
+
 
     sf::Event event;
-    // Start the game loop
     while (window.isOpen()){
         //  Check if game is over and if the restart button is clicked then reset the game.
         while (window.pollEvent(event)){
@@ -155,7 +156,7 @@ int main(){
             else if (isGameOver and event.type == sf::Event::MouseButtonPressed and event.mouseButton.button == sf::Mouse::Left) {
                 sf::Vector2i mousePos = sf::Mouse::getPosition(window);
                 if (restartButton.getGlobalBounds().contains(mousePos.x, mousePos.y)){
-                    for (int i=0;i<100;i++) {
+                    for (int i=0;i<MAX_ASTEROIDS;i++) {
                         if (i<3)
                             lifeSlots[i].setTexture(fullHeart);
                         asteroids[i].x = -100;
@@ -169,7 +170,7 @@ int main(){
                     player1.lives = 6;
                     bulletCount = 0;
                     asteroidIndex = 0;
-                    scoreTimer.restart();
+                    overallTimer.restart();
                     speedTimer.restart();
                     asteroidSpawn.restart();
                 }
@@ -178,8 +179,11 @@ int main(){
         window.clear();
         if (speedTimer.getElapsedTime().asSeconds() > 10) {
             asteroidSpeed++;
+            if (asteroidSpawnRate >= 400)
+                asteroidSpawnRate -= 100;
             speedTimer.restart();
         }
+
         // Game Over, Player runs out of lives.
         if (player1.lives == 0) {
             player1.lastScore = player1.score;
@@ -228,8 +232,11 @@ int main(){
         shootArea.setOutlineThickness(4);
         shootArea.setOutlineColor(sf::Color::White);
         scoreText.setString("Score: " + to_string(player1.score));
+        timerText.setString(to_string( (int) overallTimer.getElapsedTime().asSeconds()) );
+        timerText.setPosition(240 - timerText.getGlobalBounds().width/2, 0);
 
-    // Background Image
+
+        // Background Image
         sf::Texture background;
         background.loadFromFile("background.jpg");
         window.draw(sf::Sprite(background));
@@ -238,23 +245,43 @@ int main(){
             window.draw(lifeSlots[i]);
 
 
-    // Asteroid Proccessing
-        if (asteroidSpawn.getElapsedTime().asMilliseconds() > asteroids[0].spawnRate) { // Time to spawn a new asteroid
-            for (int i=0;i<MAX_ASTEROIDS;i++)
-                if (speedTimer.getElapsedTime().asSeconds() > 5) { // Increase difficulty after some time
-                    if (asteroids[asteroidIndex].size == 1) {
-                        asteroids[asteroidIndex].spawn();
-                    }
+        // Asteroid Proccessing
+        if (asteroidSpawn.getElapsedTime().asMilliseconds() > asteroidSpawnRate) { // Time to spawn a new asteroid
+            if (overallTimer.getElapsedTime().asSeconds() > 60) {   //  After 60 seconds, spawn only small asteroids
+                while (asteroids[asteroidIndex].size != 0) {
+                    asteroidIndex++;
+                    if (asteroidIndex >= MAX_ASTEROIDS)
+                        asteroidIndex = 0;
                 }
-            asteroidIndex++;
+            }
+            else if (overallTimer.getElapsedTime().asSeconds() > 30) {  //  After 30 seconds, spawn only small and medium asteroids
+                while (asteroids[asteroidIndex].size != 0 and asteroids[asteroidIndex].size != 1) {
+                    asteroidIndex++;
+                    if (asteroidIndex >= MAX_ASTEROIDS)
+                        asteroidIndex = 0;
+                }
+            }
+            else {                                                      //  Before 30 seconds, don't spawn small asteroids
+                while (asteroids[asteroidIndex].size != 2 and asteroids[asteroidIndex].size != 1) {
+                    asteroidIndex++;
+                    if (asteroidIndex >= MAX_ASTEROIDS)
+                        asteroidIndex = 0;
+                }
+            }
 
-            if (asteroidIndex == 100)
+            asteroids[asteroidIndex].spawn();
+            asteroidIndex++;
+            // cout << asteroidIndex << endl;
+
+            if (asteroidIndex >= MAX_ASTEROIDS) {
+                cout << "Reset AsteroidIndex" << endl;
                 asteroidIndex = 0;
+            }
 
             asteroidSpawn.restart();
         }
 
-        for (int i=0;i<100;i++) {
+        for (int i=0;i<MAX_ASTEROIDS;i++) {
             if (asteroids[i].y != -100 and asteroids[i].x != -100) {
                 asteroids[i].asteroidSprite.setPosition(asteroids[i].x, asteroids[i].y);
                 window.draw(asteroids[i].asteroidSprite);
@@ -263,29 +290,35 @@ int main(){
             if (asteroids[i].y > 800) {
                 asteroids[i].x = -100;
                 asteroids[i].y = -100;
-                player1.lives--;
+                if (!godMode)
+                    player1.lives--;
                 if (player1.lives % 2 == 0) lifeSlots[player1.lives/2].setTexture(emptyHeart);
                 else lifeSlots[player1.lives/2].setTexture(halfHeart);
             }
         }
 
 
-    //  Mouse Tracker
+        //  Mouse Tracker
         sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
         player1.x = mousePosition.x, player1.y = mousePosition.y;
         // cout << "X: " << player1.x << " Y: " << player1.y << endl;
 
-    //  Update Bullet Positions
+        //  Update Bullet Positions
+        player1.bullets[7].x = -100;    //  For some reason bullet 7's x coord gets changed to a random number, so I reset it here
+
         for (int i = 0; i < 100; i++) {
-            if (player1.bullets[i].y < 0) {
-                player1.bullets[i].x = -100;
-                player1.bullets[i].y = -100;
-                continue;
+
+            if (player1.bullets[i].x != -100 and player1.bullets[i].y != -100) {
+                if (player1.bullets[i].y < 0) {
+                    player1.bullets[i].x = -100;
+                    player1.bullets[i].y = -100;
+                    continue;
+                }
+                player1.bullets[i].y -= 10;
+                bulletShape.setPosition(player1.bullets[i].x, player1.bullets[i].y);
+                window.draw(bulletShape);
+                cout << "Bullet: " << i+1 << " (" << player1.bullets[i].x << ", " << player1.bullets[i].y << ")" <<  endl;
             }
-            player1.bullets[i].y -= 10;
-            bulletShape.setPosition(player1.bullets[i].x, player1.bullets[i].y);
-            window.draw(bulletShape);
-            cout << "Bullet: " << i+1 << " Y: " << player1.bullets[i].y << endl;
         }
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
@@ -300,8 +333,8 @@ int main(){
         }
 
         // Collision Detection
-        for (int i=0;i<100;i++) {
-            for (int j=0;j<100;j++) {
+            for (int i=0;i<100;i++) {
+            for (int j=0;j<MAX_ASTEROIDS;j++) {
                 bool reset = false;
                 if (asteroids[j].size == 0) {
                     if (player1.bullets[i].x+10 > asteroids[j].x and player1.bullets[i].x < asteroids[j].x + 20
@@ -325,7 +358,11 @@ int main(){
                     }
                 }
                 if (reset) {
-                    player1.score += 10;
+                    if (asteroids[j].size == 2) player1.score += 10;
+                    else if (asteroids[j].size == 1) player1.score += 30;
+                    else player1.score += 50;
+
+
                     player1.bullets[i].x = -100;
                     player1.bullets[i].y = -100;
                     asteroids[j].x = -100;
@@ -336,12 +373,11 @@ int main(){
 
         window.setSize({480, 800});
         window.draw(scoreText);
+        window.draw(timerText);
         window.display();
 
     }
-    int score = scoreTimer.getElapsedTime().asMilliseconds();
     cout << endl << "Bullets Fired: " << bulletCount << endl;
-    cout << "Score: " << thousandSeparator(score) << endl;
 
     return EXIT_SUCCESS;
 }
